5f5cb6577c5c379f56fe530e386eadf0566ac970
john <john.choiniere@gmail.com>
2020-01-29 15:59:33 -0800

    removed extraneous text

--- src/lib/components/DatepickerByInclusion.react.js
+++ src/lib/components/DatepickerByInclusion.react.js
@@ -33,7 +33,6 @@ export default class DatepickerByInclusion extends Component {
 
         return (
             <div id={id}>
-                ExampleComponent: &nbsp;
                 <DatePicker
                     selected={this.state.date}
                     onChange={
