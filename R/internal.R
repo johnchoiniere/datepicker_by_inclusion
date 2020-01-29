8dc8b1a47b8ab12a9a778ec23fd9dbf32b8dba1b
john <john.choiniere@gmail.com>
2020-01-28 23:23:19 -0800

    initial commit

---
+++ R/internal.R
@@ -0,0 +1,15 @@
+.datepickerByInclusion_js_metadata <- function() {
+deps_metadata <- list(`datepicker_by_inclusion` = structure(list(name = "datepicker_by_inclusion",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'datepicker_by_inclusion.min.js',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "datepickerByInclusion",
+all_files = FALSE), class = "html_dependency"),
+`datepicker_by_inclusion` = structure(list(name = "datepicker_by_inclusion",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'datepicker_by_inclusion.min.js.map',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "datepickerByInclusion",
+all_files = FALSE), class = "html_dependency"))
+return(deps_metadata)
+}
---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+import DatepickerByInclusion from './components/DatepickerByInclusion.react';
+
+export {
+    DatepickerByInclusion
+};
